6912a233fa94017507dd43486f6bc82fe9c57f52
Chris Proffitt <chrisp@wncit.com>
2019-09-13 10:36:22 -0400

    first GAY commit

---
+++ header.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html class="no-js" lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="x-ua-compatible" content="ie=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Rude Motorsports</title>
+	<link rel="stylesheet" href="css/app.css">
+</head>
+<body>
+	<div class="header-container">
+		<div class="grid-container nav-container grid-x">
+			<div class="large-2 nav-logo">
+				<img style="width: 100%;" src="images/logo/rude-logo.png">
+			</div>
+			<div class="large-10">
+				hey
+			</div>
+		</div>
+	</div>
+	<div class="grid-container">
+      
+     
---
+++ index.php
@@ -0,0 +1,64 @@
+<?php include 'header.php'; ?>
+    
+<div class="grid-x">
+  <div class="large-6">
+    hey
+  </div>
+    <div class="large-6">
+    hey
+  </div>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<?php include 'footer.php'; ?>
