12f8c4f30d6c0f17f93b6bda513cbf573e1f018e
Chris Proffitt <chrisp@wncit.com>
2019-09-16 10:19:40 -0400

    going in a little bit more salt cave direction

--- header.php
+++ header.php
@@ -9,20 +9,10 @@
 </head>
 <body>
 	<div class="header-container">
-		<div class="grid-container nav-container grid-x">
-			<div class="shrink nav-logo">
-				<img style="width: 100%;" src="images/logo/rude-logo.png">
-			</div>
-			<!-- <div class="large-10 nav-items-wrapper grid-x"> -->
-				<div class="grid-x large-10">
-					<div class="cell auto">&nbsp;</div>
-					<div class="cell shrink nav-item">Home</div>
-					<div class="cell shrink nav-item">Contact</div>
-					<div class="cell shrink nav-item">Media</div>
-					<div class="cell shrink nav-item">Schedule</div>
-				</div>
-			<!-- </div> -->
+		<div class="top-nav-container">
+			hey
 		</div>
+		
 	</div>
 	<div class="grid-container">
       
